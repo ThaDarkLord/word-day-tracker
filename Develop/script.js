c3fd110ea5565e3cbca1b22b82ff5d672f9eb73f
ThaDarkLord <daniel.j.sykes92@gmail.com>
2024-01-06 18:22:04 -0500

    finished logic in javascript pertaining to button and saving items in local storage also created a for loop to change colors according to time

--- Develop/script.js
+++ Develop/script.js
@@ -1,23 +1,116 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-});
+// Variable Declaration
+var todayDate = $('#currentDay');
+var textArea = document.querySelectorAll('.description'); // this line is going to be used to change the color of the box for the current hour of the day in the for loop!
+var time = dayjs().hour()
+var saveBtn9 = $('#saveBtn9');
+var saveBtn10 = $('#saveBtn10');
+var saveBtn11 = $('#saveBtn11');
+var saveBtn12 = $('#saveBtn12');
+var saveBtn13 = $('#saveBtn13');
+var saveBtn14 = $('#saveBtn14');
+var saveBtn15 = $('#saveBtn15');
+var saveBtn16 = $('#saveBtn16');
+var saveBtn17 = $('#saveBtn17');
+
+// current date and time function
+function date(){
+ setInterval(function(){
+  var currentTime = dayjs().format('MMM DD, YYYY [at] HH:mm:ss a')
+ todayDate.text(currentTime);
+
+}, 1000)
+}
+date()
+
+//  these functions save the item content to the local storage upon the buttton being clicked
+function saveBtn09(){
+  saveBtn9.on('click', function(){
+    var input = $('#input9').val()
+    localStorage.setItem('hour-9', input)
+    
+  })
+
+}
+saveBtn09()
+
+function saveBtn010(){
+  saveBtn10.on('click', function(){
+    var input = $('#input10').val()
+    localStorage.setItem('hour-10', input)
+  })
+}
+saveBtn010()
+
+function saveBtn011(){
+  saveBtn11.on('click', function(){
+    var input = $('#input11').val()
+    localStorage.setItem('hour-11', input)
+  })
+}
+saveBtn011()
+
+function saveBtn012(){
+  saveBtn12.on('click', function(){
+    var input = $('#input12').val()
+    localStorage.setItem('hour-12', input)
+  })
+}
+saveBtn012()
+
+function saveBtn013(){
+  saveBtn13.on('click', function(){
+    var input = $('#input13').val()
+    localStorage.setItem('hour-13', input)
+  })
+}
+saveBtn013()
+
+function saveBtn014(){
+  saveBtn14.on('click', function(){
+    var input = $('#input14').val()
+    localStorage.setItem('hour-14', input)
+  })
+}
+saveBtn014()
+
+function saveBtn015(){
+  saveBtn15.on('click', function(){
+    var input = $('#input15').val()
+    localStorage.setItem('hour-15', input)
+  })
+}
+saveBtn015()
+
+function saveBtn016(){
+  saveBtn16.on('click', function(){
+    var input = $('#input16').val()
+    localStorage.setItem('hour-16', input)
+  })
+}
+saveBtn016()
+
+function saveBtn017(){
+  saveBtn17.on('click', function(){
+    var input = $('#input17').val()
+    localStorage.setItem('hour-17', input)
+  })
+}
+saveBtn017()
+
+// this for loop goes through a;; of the text areas with the class of description and converts the number corresponding with the data-time attribute so that I can check the time against the respected section.
+for (var i = 0; i < textArea.length; i++) {
+  var currentTextArea = textArea[i];
+  var textAreaData = parseInt(currentTextArea.getAttribute('data-time'));
+  if(time < textAreaData){
+    currentTextArea.classList.add('future')
+  } else if(time > textAreaData){
+    currentTextArea.classList.add('past')
+  } else{
+    currentTextArea.classList.add('present')
+  }
+  
+}
+  
+  
+
+
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # word-day-tracker
+
+## Description
+This project is a work day planner that will allow the users text to be storage within the local storage for future use.  The page is color coded based on the time of day if the time is outside of normal business hours the text slot will appear grey. If the time is current it will appear red and if the time has not arrived the text slot will appear green. 
+
+
+
+## Usage
+The user can type in the text area a message for work they would like saved within that slot and it will save to the local storage.
+
+![image of work day planner](./Assets/img/Screenshot%202024-01-06%20at%206.07.37 PM.png)
+
